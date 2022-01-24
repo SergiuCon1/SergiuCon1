5f241e8cb0185e25c779cc0e3530623c47307d87
SergiuCon1 <98112867+SergiuCon1@users.noreply.github.com>
2022-01-24 10:11:43 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,101 @@
+# a=int(input())
+# if a % 2 == 0:
+#   print('a - четное')
+# else:
+#   print('a - нечетное')
+
+# height = int(input())
+
+# if height < 170:
+#   print('В танкисты')
+# elif height < 185:
+#   print('на флот')
+# elif height < 200:
+#   print('в десантники')
+# else:
+#     print('в другие войска')
+
+# height = int(input())
+
+# if height < 170:
+#   print('В танкисты')
+# if height < 185:
+#   print('на флот')
+# if height < 200:
+#   print('в десантники')
+
+# name = input('Enter you name')
+
+# if name:
+#   print('Hello', name)
+# else:
+#   print('Hello, world')
+
+# number = input('Enter the number')
+
+# if number:
+#   print('Number', number)
+# else:
+#   print('Number is null')
+
+# print(True and True)
+# print(True and False)
+# print(True or False)
+# print(not True)
+
+# print((9 > 2) and (2 < 4))
+# print((8 == 9) or (6 != 6))
+
+# age = int(input('Enter your age'))
+# height = int(input('Enter your height'))
+
+# if height < 170 and 16 <= age <= 18:
+#   print('Ready for military')
+# elif height > 170 or age > 18:
+#   print('also ready')
+# else:
+#   print('Not ready')
+
+# height = int(input('Рост'))
+# age = int(input('Возраст'))
+
+# if age >= 18:
+#   if height < 170:
+#     print('В танкисты')
+#   if height < 185:
+#     print('на флот')
+#   if height < 200:
+#     print('в десантники')
+#   else:
+#     print('В другие войска')
+# else:
+#   print('Не призывной')
+
+# account_amout = 9000
+# loan_amout = 10000
+# days_to_pay = 10
+# is_holiday = False
+
+# if account_amout < loan_amout and (days_to_pay < 10 and not is_holiday) or days_to_pay == 15:
+#   print('Верни кредит сука')
+# else:
+#   print(False)
+
+# year = int(input('Введите год'))
+
+# if year % 4 != 0:
+#   print('Обычный год')
+# elif year % 100 == 0:
+#   if year % 400 == 0:
+#     print('Высокосный')
+#   else:
+#     print('Обычный')
+# else:
+#   print('Высокосный')
+
+# year = int(input('Введите год'))
+
+# if year % 4 != 0 or (year % 100 == 0 and year % 400 == 0):
+#     print('Обычный')
+# else:
+#     print('Высокосный')
